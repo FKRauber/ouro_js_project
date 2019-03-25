c3be64c2aa4ef90ea315faf018f82384685f03b4
FKRauber <fabienne.rauber@gmail.com>
2019-03-25 11:13:59 -0400

    completed show_link ; modularized getTreasures

--- app/assets/javascripts/treasures.js
+++ app/assets/javascripts/treasures.js
@@ -1,10 +1,6 @@
 $(() => {
   console.log("treasures.js loaded")
-
-  bindClickHandlers()
-  // attachListeners();
-  // listenForNewFormClick()
-  // listenForNextClick()
+  bindClickHandlers();
 });
 
 const bindClickHandlers = () => {
@@ -13,110 +9,67 @@ const bindClickHandlers = () => {
     history.pushState(null, null, "treasures_list");
     console.log('default prevented at all_treasures');
 
-    fetch(`/treasures.json`)
-      .then(res => res.json())
-        .then(treasures => {
-          $('#app-container').html('')
-          treasures.forEach(treasure => {
-            let newTreasure = new Treasure(treasure)
-            let treasureHTML = newTreasure.formatIndex();
-            $('#app-container').append(treasureHTML);
-          });
-        });
+    getTreasures();
   });
-
   $(document).on('click', ".show_link", function(e) {
     // above assigns parent as show_link is not in DOM until we click it
     e.preventDefault();
     console.log('default prevented at show_link');
-    console.log(this);
-    fetch(`/treasures.json`)
+    let id = $(this).attr('data_id');
+    fetch(`/treasures/${id}.json`)
+      .then(res => res.json())
+        .then(treasure => {
+          $('#app-container').html('')
+          let newTreasure = new Treasure(treasure)
+          let treasureHTML = newTreasure.formatShow();
+          $('#app-container').append(treasureHTML);
+        });
   });
 }
 
-// function attachListeners() {
-//   $('button#ajax-theory-data').on('click', () => getTreasures());
-//   $('button#ajax-new-treasure').on('click', () => newTreasureForm());
-//   $('button#ajax-next-treasure').on('click', () => getNextTreasure());
-// }
-
-// function listenForNewFormClick() {
-//   $('button#ajax-new-treasure').on('click', function(event) {
-//     event.preventDefault()
-//     newTreasureForm();
-// **    document.querySelector('div#ajax-new-treasure-form').innerHTML = newTreasureForm
-//   })
-// }
-
-// function listenForNextClick() {
-//   $('button#ajax-next-treasure').on('click', function(event) {
-//     event.preventDefault()
-//     getNextTreasure()
-//   })
-// }
-
-function getTreasures() {
-  $.ajax({
-    url: 'http://localhost:3000/treasures',
-    method: 'get',
-    dataType: 'json'
-  }).done(function(data) {
-    console.log("the data is...", data)
-
-    for (var i = 0; i < data.length; i++) {
-      let myTreasure = new Treasure(data[0])
-      let myTreasureHTML = myTreasure.postHTML()
-      $('div#ajax-treasures').html(myTreasureHTML)
-      document.getElementById('ajax-treasures').innerHTML = myTreasureHTML
-    }
-
-  })
-}
-
-function getNextTreasure() {
-  $.ajax({
-    url: 'http://localhost:3000/treasures',
-    method: 'get',
-    dataType: 'json'
-  }).done(function(data) {
-    console.log("the data is...", data)
-    var nextId = parseInt($("#ajax-next-treasure").attr("data-id")) + 1;
-    debugger;
-    $.get("/treasures/" + nextId + ".json", function(data) {
-      $("#ajax-next-treasure").attr("data-id", data["id"]);
-    });
-  });
-}
-
-function newTreasureForm() {
-    $.ajax({
-      url: 'http://localhost:3000/treasures',
-      method: 'post',
-      dataType: 'json'
-    }).done(function() {
-      $('form').submit(function(event) {
-        event.preventDefault();
-        console.log("creating new treasure")
-
-        var values = $(this).serialize();
-        var posting = $.post('/treasures', values);
-        posting.done(function(data){
-          console.log("the data is...")
+const getTreasures = () => {
+  fetch(`/treasures.json`)
+    .then(res => res.json())
+      .then(treasures => {
+        $('#app-container').html('')
+        treasures.forEach(treasure => {
+          let newTreasure = new Treasure(treasure)
+          let treasureHTML = newTreasure.formatIndex();
+          $('#app-container').append(treasureHTML);
         });
       });
-      // document.querySelector('div#ajax-new-treasure-form').innerHTML = newTreasureForm
-      // console.log(newTreasureForm);
+}
 
-      return (`
-        <strong>New Treasure Form</strong>
-        <form>
-          <input id="treasure-title" type="text" name="title" placeholder="Name"></input><br>
-          <input type="text" name="description" placeholder="Description"></input><br>
-          <input type="submit" />
-        </form>
-      `)
-    });
-  }
+
+// function newTreasureForm() {
+//     $.ajax({
+//       url: 'http://localhost:3000/treasures',
+//       method: 'post',
+//       dataType: 'json'
+//     }).done(function() {
+//       $('form').submit(function(event) {
+//         event.preventDefault();
+//         console.log("creating new treasure")
+//
+//         var values = $(this).serialize();
+//         var posting = $.post('/treasures', values);
+//         posting.done(function(data){
+//           console.log("the data is...")
+//         });
+//       });
+//       // document.querySelector('div#ajax-new-treasure-form').innerHTML = newTreasureForm
+//       // console.log(newTreasureForm);
+//
+//       return (`
+//         <strong>New Treasure Form</strong>
+//         <form>
+//           <input id="treasure-title" type="text" name="title" placeholder="Name"></input><br>
+//           <input type="text" name="description" placeholder="Description"></input><br>
+//           <input type="submit" />
+//         </form>
+//       `)
+//     });
+//   }
 
 function Treasure(treasure) {
   this.id = treasure.id
@@ -128,7 +81,15 @@ function Treasure(treasure) {
 
 Treasure.prototype.formatIndex = function() {
   let treasureHTML = `
-    <a href="/treasures/${this.id}" class="show_link">${this.name}</a><br><br><br>
+    <a href="/treasures/${this.id}" class="show_link" data_id="${this.id}">${this.name}</a><br><br><br>
+    `
+
+  return treasureHTML;
+}
+
+Treasure.prototype.formatShow = function() {
+  let treasureHTML = `
+    <h3>${this.name}</h3>
     `
 
   return treasureHTML;
